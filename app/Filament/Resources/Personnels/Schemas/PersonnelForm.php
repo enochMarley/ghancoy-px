5c6e407e968100866cc8059ddff8e035d29e8779
megakharisdev <megakharisdev@gmail.com>
2025-10-15 19:26:29 +0000

    Update on the PersonnelForm

--- app/Filament/Resources/Personnels/Schemas/PersonnelForm.php
+++ app/Filament/Resources/Personnels/Schemas/PersonnelForm.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Personnels\Schemas;
 use App\Enum\PersonnelGender;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
 
@@ -14,38 +15,41 @@ class PersonnelForm
     {
         return $schema
             ->components([
-                TextInput::make('service_number')
-                    ->required()
-                    ->columnSpan(4),
+                Section::make()
+                    ->schema([
+                        TextInput::make('service_number')
+                            ->required()
+                            ->columnSpan(6),
 
-                Select::make('rank_id')
-                    ->relationship('rank', 'code')
-                    ->required()
-                    ->columnSpan(4),
+                        Select::make('rank_id')
+                            ->relationship('rank', 'code')
+                            ->required()
+                            ->columnSpan(6),
 
-                TextInput::make('last_name')
-                    ->required()
-                    ->columnSpan(4),
+                        TextInput::make('last_name')
+                            ->required()
+                            ->columnSpan(6),
 
-                TextInput::make('other_names')
-                    ->required()
-                    ->columnSpan(4),
+                        TextInput::make('other_names')
+                            ->required()
+                            ->columnSpan(6),
 
-                Select::make('gender')
-                    ->options(PersonnelGender::class)
-                    ->default('Male')
-                    ->required()
-                    ->columnSpan(4),
+                        Select::make('gender')
+                            ->options(PersonnelGender::class)
+                            ->default('Male')
+                            ->required()
+                            ->columnSpan(6),
 
-                PhoneInput::make('phone')
-                    ->initialCountry('gh')
-                    ->columnSpan(4),
+                        PhoneInput::make('phone')
+                            ->initialCountry('gh')
+                            ->columnSpan(6),
 
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->columnSpan(4),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->columnSpan(6),
+                    ])->columns(12)
 
-            ])->columns(12);
+            ])->columns(1);
     }
 }
